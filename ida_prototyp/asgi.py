ff3b6de5f71226548c276fb285ddb764fee90d5c
rafaelbeier <rafael.beier@bundesbank.de>
2020-07-08 17:15:39 +0200

    Initial Commit

---
+++ ida_prototyp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ida_prototyp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ida_prototyp.settings')
+
+application = get_asgi_application()
