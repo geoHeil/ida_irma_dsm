75406a8572ad3464b1f33086e137c9eda05853f9
Constantin Brincoveanu <c.brincoveanu@yahoo.com>
2020-07-13 15:22:15 +0200

    add database and datasetfamily

--- ida_core/admin.py
+++ ida_core/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Consumer
+from .models import Consumer, Database, DatasetFamily
 
 admin.site.register(Consumer)
+admin.site.register(Database)
+admin.site.register(DatasetFamily)
 
 admin.site.site_header = 'IDA/IRMA/DSM administration'
--- ida_core/models.py
+++ ida_core/models.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import User
 class Consumer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+
+class Database(models.Model):
+    name = models.CharField(max_length=100)
+
+class DatasetFamily(models.Model):
+    name = models.CharField(max_length=100)
+    database = models.ForeignKey(Database, on_delete=models.CASCADE)
