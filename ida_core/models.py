3b97e56571d2d6d38e7ecbf1ca4c82bbcf18d8dc
Constantin Brincoveanu <c.brincoveanu@yahoo.com>
2020-07-13 14:52:39 +0200

    add consumer user relation

--- ida_core/models.py
+++ ida_core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Consumer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
