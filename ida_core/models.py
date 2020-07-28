f342a303b19a7a89801a58b2a821ef8924c348e5
Constantin Brincoveanu <c.brincoveanu@yahoo.com>
2020-07-28 13:03:46 +0200

    move research purpose to project

--- ida_core/models.py
+++ ida_core/models.py
@@ -101,6 +101,7 @@ class Project(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     project_lead = models.ForeignKey(Consumer, on_delete=models.PROTECT)
+    access_mode_research_field = models.ForeignKey(AccessModeResearchField, on_delete=models.PROTECT)
 
     def __str__(self):
             return self.name
@@ -113,7 +114,6 @@ class ProjectGroup(models.Model):
     dataset_families = models.ManyToManyField(DatasetFamily)
     access_mode_type = models.ForeignKey(AccessModeType, on_delete=models.PROTECT)
     access_mode_anonymization = models.ForeignKey(AccessModeAnonymization, on_delete=models.PROTECT)
-    access_mode_research_field = models.ForeignKey(AccessModeResearchField, on_delete=models.PROTECT)
 
     def status(self):
         # Get required achievements
@@ -121,7 +121,7 @@ class ProjectGroup(models.Model):
         required_achievements = []
         for i in dsf:
             access_regime = i.access_regime
-            access_modes = access_regime.accessmode_set.filter(access_mode_type=self.access_mode_type).filter(access_mode_anonymization=self.access_mode_anonymization).filter(access_mode_research_field=self.access_mode_research_field)
+            access_modes = access_regime.accessmode_set.filter(access_mode_type=self.access_mode_type).filter(access_mode_anonymization=self.access_mode_anonymization).filter(access_mode_research_field=self.project.access_mode_research_field)
             if access_modes.count() == 0:
                 return f"{i} has no suitable access mode available."
             else:
